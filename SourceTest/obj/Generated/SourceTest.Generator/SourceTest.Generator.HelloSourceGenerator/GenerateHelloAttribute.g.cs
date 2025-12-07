215899361041bc13ad7e80f187030efbadd60d53
dsrice <v.fuji.0223@gmail.com>
2025-12-07 23:20:34 +0900

    chore: Remove HelloSourceGenerator and update .gitignore

--- SourceTest/obj/Generated/SourceTest.Generator/SourceTest.Generator.HelloSourceGenerator/GenerateHelloAttribute.g.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SourceTest.Generator
-{
-    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-    public sealed class GenerateHelloAttribute : Attribute
-    {
-    }
-}
